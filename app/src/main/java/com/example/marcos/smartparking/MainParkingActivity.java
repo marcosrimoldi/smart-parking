3325cf8ed813b5a43df2f77cf8300eafe369249c
Marcos Rimoldi <marcos.rimoldi@gmail.com>
2017-06-25 13:04:59 -0300

    Hide keyboard on starting parking.

--- app/src/main/java/com/example/marcos/smartparking/MainParkingActivity.java
+++ app/src/main/java/com/example/marcos/smartparking/MainParkingActivity.java
@@ -3,6 +3,7 @@ package com.example.marcos.smartparking;
 import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
@@ -16,6 +17,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -56,6 +58,8 @@ public class MainParkingActivity extends AppCompatActivity implements ParkingSer
                     final EditText input = (EditText) findViewById(R.id.domainText);
                     locationPostService.postStartParking(new ParkingDTO(input.getText().toString(),
                             getLastKnownLocation()));
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(input.getWindowToken(), 0);
                     progress.show();
                 }
             }
